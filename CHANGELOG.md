c6860e375547f638542bba83ad77ae63eab54c2b
Dmitry Mozzherin <dmozzherin@gmail.com>
2014-08-20 19:21:40 -0400

    working version

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -3,9 +3,14 @@ eol-ufw-wrapper CHANGELOG
 
 This file is used to list changes made in each version of the eol-ufw-wrapper cookbook.
 
+0.1.2
+-----
+- [Dmitry Mozzherin][1] - bug fix
+
 0.1.1
 -----
 - [Dmitry Mozzherin][1] - implements rules taken from data bag
+
 0.1.0
 -----
 - [Dmitry Mozzherin][1] - Initial nonfunctional release of eol-ufw-wrapper
--- recipes/default.rb
+++ recipes/default.rb
@@ -3,22 +3,22 @@
 # Recipe:: default
 #
 
-require 'ostruct'
+require "ostruct"
 
-include_recipe('ufw')
-
-ufw_rules = data_bag_item('eol-ufw-wrapper', 'config') rescue {}
+include_recipe("ufw")
 
 def create_firewal_rule(rule_name, data)
   firewall_rule rule_name do
     name rule_name
-    protocol data.protocol if data.protocol
+    protocol data.protocol.to_sym if data.protocol
     port data.port.to_i if data.port
-    action data.action if data.action
+    action data.action.to_sym if data.action
   end
 end
 
-ufw_rules['ufw_rules'].each do |name, data|
+ufw_rules = data_bag_item("eol-ufw-wrapper", "config") 
+
+ufw_rules["ufw_rules"].each do |name, data|
   data = OpenStruct.new(data)
   if data.nodes.empty? || data.nodes.include?(node.name)
     create_firewal_rule(name, data) 
